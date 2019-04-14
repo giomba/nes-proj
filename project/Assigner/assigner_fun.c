6c01ac8d38189e8221013974c14bc5f5d22a2967
Daniela <danielacomola@gmail.com>
2019-04-14 18:24:52 +0200

    [Assigner] Bugs fixed in handling mac addresses. Some other adjustments in Assigner module

--- project/Assigner/assigner_fun.c
+++ project/Assigner/assigner_fun.c
@@ -2,26 +2,18 @@
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include "sys/log.h"
-#include "sys/clock.h"
-#include "sys/ctimer.h"
 #include "os/dev/leds.h"
-#include "os/dev/serial-line.h"
-#include "arch/cpu/cc26x0-cc13x0/dev/cc26xx-uart.h"
 
-#include "../msg.h"
+#include "../common/supermarket_net.h"
 #include "assigner_fun.h"
 
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-extern cart* cart_list;
 
-//struct cart cart;
-
-//function invoked in order to looking for the most charged cart to assign to the new arrived client	
+//function invoked in order to look for the most charged cart to assign to the new arrived client	
 cart* cart_selection()
 {	
 	uint8_t highest_battery = 0;
@@ -29,7 +21,7 @@ cart* cart_selection()
 	cart* current = cart_list;
 	while(current)
 	{
-		if(!current->assigned && current->battery_status > highest_battery)
+		if(!(current->assigned) && (current->battery_status > highest_battery))
 		{
 			highest_battery = current->battery_status;
 			selected = current;
@@ -40,7 +32,7 @@ cart* cart_selection()
 }
 
 //Insert a new cart in the list with the battery info just arrived
-bool insert_cart(uint8_t new_req_battery, linkaddr_t* mac_cart_addr)
+bool insert_cart(uint8_t new_req_battery, linkaddr_t mac_cart_addr)
 {
 	cart* new_arrived_cart = (cart*)malloc(sizeof(cart));
 	if(new_arrived_cart==NULL)
@@ -55,22 +47,25 @@ bool insert_cart(uint8_t new_req_battery, linkaddr_t* mac_cart_addr)
 		new_arrived_cart->assigned = false;
 		new_arrived_cart->next = cart_list;
 		cart_list = new_arrived_cart;
+		LOG_INFO("Nuovo carrello inserito con mac_address: ");
+		LOG_INFO_LLADDR(&(new_arrived_cart->cart_address));
+		printf("\n");
 	}
 	return true;
 }
 
 //Upgrade the battery status of a cart
-bool bat_upgrade(linkaddr_t* src_cart_addr, uint8_t battery_level)
+bool bat_update(linkaddr_t src_cart_addr, uint8_t battery_level)
 {
 	cart* c = cart_list;
-	//for(c; c && (c->cart_address != src_cart_addr); c = c->next);
+	//looking for cart->address = address of who sent the message with the battery 
 	while(c)
 	{
-		if(c->cart_address != src_cart_addr)
+		if(linkaddr_cmp(&c->cart_address,&src_cart_addr) == 0)
 			c = c->next;
 		else
 			break;
-	} 
+	}
 	if(!c)
 	{
 		LOG_INFO("Cart not associated yet!\n");
@@ -79,6 +74,62 @@ bool bat_upgrade(linkaddr_t* src_cart_addr, uint8_t battery_level)
 	c->battery_status = battery_level;
 	c->assigned = false;  //a battery status is sent only when the cart is in his place, not with a client. So if the cart was out and the the battery status is received, it is now come back in place. 
 	return true;	
+}
+
+void handle_association_request(linkaddr_t src, const void* data)
+{
+	//turn on the led for association msg received
+	assoc_req_msg ar_msg;
+	memcpy (&ar_msg, data, sizeof ((assoc_req_msg *)data));
+	if(insert_cart(ar_msg.battery_percentage, src))
+	{
+		assoc_reply_msg notification;
+		notification.msg_type = ASSOCIATION_REPLY_MSG;
+		LOG_INFO("\n");
+		LOG_INFO("Sending acknowledgment of successfull association\n");
+	
+		nullnet_buf = (uint8_t*)&notification;
+		nullnet_len = sizeof(notification);
+		NETSTACK_NETWORK.output(&src);
+	}
+	LOG_INFO("New cart associated\n");
+}
+
+void handle_battery_msg(linkaddr_t src, const void* data)
+{
+	//turn on the led for battery msg received
+	battery_msg bt_msg;
+	memcpy (&bt_msg, data, sizeof ((battery_msg *)data));
+	if(bat_update(src, bt_msg.battery_percentage))
+	{
+		LOG_INFO("Battery level upgraded of ");
+		LOG_INFO_LLADDR(&src);
+		printf("\n");	
+		printf("Battery: %d\n", bt_msg.battery_percentage);
+	}	
+}
+
+//callback function for the ctimer that checks if all the carts have been replaced when the supermarket close
+void check(void *ptr)
+{
+	supermarket_open = !supermarket_open;
+	leds_toggle(LEDS_ALL);
+	if(!supermarket_open)
+	{
+		printf("Supermarket is closed!\n");		
+		cart* c = cart_list;
+		//the assigner checks if all the carts are back in their place or are still out
+		while(c)
+		{	
+			if(c->assigned)
+				printf("Customer id %d hasn't replaced his cart\n", (int)c->customer_id);
+			c = c->next;
+		}
+	}
+	else
+		printf("Supermarket is open!\n");
+	process_poll(&assigner_process);
+		
 }
 
 
--- project/Assigner/assigner_fun.h
+++ project/Assigner/assigner_fun.h
@@ -5,23 +5,24 @@
 #include <string.h>
 #include <stdlib.h>
 #include "sys/log.h"
-#include "sys/clock.h"
-#include "sys/ctimer.h"
-#include "os/dev/leds.h"
-#include "os/dev/serial-line.h"
-#include "arch/cpu/cc26x0-cc13x0/dev/cc26xx-uart.h"
+
 
 typedef struct cart
 {
-	linkaddr_t* cart_address;
+	linkaddr_t cart_address;
 	uint8_t battery_status;
 	bool assigned;
 	uint32_t customer_id;
 	struct cart *next;
 }cart;
 
+extern bool supermarket_open;
+extern cart* cart_list;
+extern struct process assigner_process;
+
 cart* cart_selection();
-bool insert_cart(uint8_t new_req_battery, linkaddr_t* mac_cart_addr);
-bool bat_upgrade(linkaddr_t* src_cart_addr, uint8_t battery_level);
-//static void input_callback(const void* data, uint16_t len, const linkaddr_t* source_address, const linkaddr_t* destination_address);
-//void check(void *ptr);
+bool insert_cart(uint8_t new_req_battery, linkaddr_t mac_cart_addr);
+bool bat_update(linkaddr_t src_cart_addr, uint8_t battery_level);
+void handle_association_request(linkaddr_t src, const void* data);
+void handle_battery_msg(linkaddr_t src, const void* data);
+void check(void *ptr);
