9df5ac697541d4ba31090bf9d1988d7d89f61188
Daniela <danielacomola@gmail.com>
2019-04-16 15:53:28 +0200

    [Assigner]some bugs fixed

--- project/Assigner/assigner_fun.c
+++ project/Assigner/assigner_fun.c
@@ -41,7 +41,7 @@ bool insert_cart(uint8_t new_req_battery, linkaddr_t mac_cart_addr)
 	while(linkaddr_cmp(&(c->cart_address),&mac_cart_addr) == 0)
 		c = c->next;
 	if(c)
-		printf("Cart already associated!"\n);
+		printf("Cart already associated!\n");
 	else
 	{
 		new_arrived_cart->cart_address = mac_cart_addr;
